2fb32fbbe3512fe410a1034b799ce351ee71f6de
bindhushree05 <141924979+bindhushree05@users.noreply.github.com>
2023-09-11 11:49:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1: Perform the addition operation (add x and y) and print the answer in the console.
+var addition = x + y;
+console.log(addition);
+
+// Challenge 2: Perform the Subtraction operation (subtract y from x) and print the answer in the console.
+var subtraction = x - y;
+console.log(subtraction);
+// Challenge 3: Perform Multiplication operation (multiply x from y) and print the answer in console.
+var multiplicaation = x*y;
+console.log(multiplicaation);
+// Challenge 4: Perform Division operation (Divide x by y) and print the answer in console.
+var division = x/y;
+console.log(division);
+// Challenge 5: perform division operation (divide x by y) and print the remainder in console.
+var remainder = x%y;
+console.log(remainder);
+// Challenge 6: Perform Exponent operation and print the answer in console.
+var y
+var z = 20;
+const result = z**y;
+console.log(result);
+// Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
+var power = x ** y;
+console.log(power);
+// Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+console.log(--z);
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var d = 20;
+
+// Challenge 9: Perform Addition assignment operation to the variable a;
+console.log(a+=5);
+// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+console.log(b-= 2)
+// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+console.log(c*=3)
+// Challenge 12: Perform 	Division assignment operation to the variable c;
+console.log(d/=4)
+// Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+var m = 4;
+var x = 4;
+var equality = a === d;
+console.log(equality);
+var equality = m === x;
+console.log(equality);
+// Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+var a = 6;
+var b = 6;
+var nonequality = a !== b;
+console.log(nonequality);
+var nonequality = b !== c;
+console.log(nonequality);
+// Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+var c = 25;
+var d = 30;
+var Lessthan = d < c;
+console.log(Lessthan);
+var Lessthan = c < d;
+console.log(Lessthan);
+// Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+var y = 40;
+var x = 15;
+var Greaterthan = y > x;
+console.log(Greaterthan);
+var Greaterthan = x > y;
+console.log(Greaterthan);
+// Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+var v = 9;
+var c = 27;
+var Lessthanequalto = d <= c;
+console.log(Lessthanequalto);
+var Lessthanequalto = v <= c;
+console.log(Lessthanequalto);
+// Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
+var n = 15;
+var m = 30;
+var Greaterthanequalto = n >= m;
+console.log(Greaterthanequalto);
+var Greaterthanequalto = d >= c;
+console.log(Greaterthanequalto);
